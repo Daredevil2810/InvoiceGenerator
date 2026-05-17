23a0100a72e6137edf1a36b31faab990d91d525c
Devanshu Panchal <panchaldevanshu2810@gmail.com>
2026-05-17 11:52:54 +0530

    changed css again and added notes again

--- invoices/forms.py
+++ invoices/forms.py
@@ -41,7 +41,7 @@ class InvoiceForm(forms.ModelForm):
             'invoice_date',
             'due_date',
             'discount',
-            'notes'
+            'note'
         ]
 
         widgets = {
