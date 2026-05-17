e4c9199f8bbf4a3df258d00a4cefeb1f46b624b4
Devanshu Panchal <panchaldevanshu2810@gmail.com>
2026-05-17 15:04:30 +0530

    changed quantity data type bug

--- invoices/models.py
+++ invoices/models.py
@@ -291,10 +291,7 @@ class InvoiceItem(models.Model):
 
     hsn_sac = models.CharField(max_length=100)
 
-    quantity = models.DecimalField(
-        max_digits=10,
-        decimal_places=2
-    )
+    quantity = models.PositiveIntegerField()
 
     rate = models.DecimalField(
         max_digits=10,
